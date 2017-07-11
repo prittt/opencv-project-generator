3b6971c0880da9e962ef428d8bec5bff82b8e153
FedericoBolelli <prittt@users.noreply.github.com>
2017-07-11 14:42:56 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
-#include <iostream>
 #include <cstdint>
 
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+
 using namespace std;
+using namespace cv;
 
 int main()
 {
-
+    Mat1b img;
+    img = imread("../data/rana.jpg", IMREAD_GRAYSCALE);
+    imshow("Example", img);
+    waitKey();
+    
     return EXIT_SUCCESS;
 }
