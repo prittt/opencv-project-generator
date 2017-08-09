e5c77e5c31f3218ca175315255360f983acbbfe5
Federico Bolelli <lk_number1@hotmail.it>
2017-08-09 09:45:46 +0200

    Add example of "imread" + "imshow" to test if OpenCV were correctly included

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#define _CRT_SECURE_NO_WARNINGS //To suppress 'fopen' opencv warning/bug 
+
 #include <cstdint>
 
 #include <iostream>
@@ -9,10 +11,27 @@ using namespace cv;
 
 int main()
 {
-    Mat1b img;
-    img = imread("../data/rana.jpg", IMREAD_GRAYSCALE);
-    imshow("Example", img);
-    waitKey();
-    
+    // Read and display grayscale image
+    Mat1b img_grayscale;
+    img_grayscale = imread("../data/rana.jpg", IMREAD_GRAYSCALE);
+    if (img_grayscale.empty()) {
+        cout << "Unable to read grayscale image" << endl;
+    }
+    else {
+        imshow("Example - Grayscale", img_grayscale);
+        waitKey();
+    }
+
+    // Read and display color image
+    Mat3b img_color;
+    img_color = imread("../data/rana.jpg", IMREAD_COLOR);
+    if (img_color.empty()) {
+        cout << "Unable to read grayscale image" << endl;
+    }
+    else {
+        imshow("Example - Color", img_color);
+        waitKey();
+    }
+
     return EXIT_SUCCESS;
 }
